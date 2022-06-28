6ac128e4fd8f286ebe493a054b23c2a78a069c63
ShaunM <32672907+Shaun-Mifsud@users.noreply.github.com>
2022-06-28 18:48:02 +0200

    images

--- main.js
+++ main.js
@@ -1380,49 +1380,54 @@ const EVENTS = [
         type: "Performance",
         location: { lat: 35.929777, lng: 14.441126 },
         date: "2022-03-24",
-        time: "12:00:00"
+        time: "12:00:00",
+        image: '/assets/images/events/feast.jpg'
     },
     {
         id: 2,
-        ensembleID: 1,
+        ensembleID: 3,
         scores: [3],
         name: "Summer Concert",
         description: "Event Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         type: "Practice",
         location: { lat: 20, lng: 20 },
         date: "2022-03-24",
-        time: "09:00:00"
+        time: "09:00:00",
+        image: '/assets/images/events/orch.jpg'
     },
     {
         id: 3,
-        ensembleID: 1,
+        ensembleID: 2,
         name: "Good Friday procession",
         description: "Event Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         type: "Performance",
         location: { lat: 20, lng: 150 },
         date: "2022-03-24",
-        time: "18:15:00"
+        time: "18:15:00",
+        image: '/assets/images/events/goodFri.jpg'
     },
     {
         id: 4,
-        ensembleID: 2,
+        ensembleID: 3,
         name: "President Concert",
         description: "Event Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         type: "Performance",
-        location: { lat: 20, lng: 20 },
+        location: { lat: 35.89973371638821, lng: 14.518266611335347 },
         date: "2022-03-24",
-        time: "10:30:00"
+        time: "10:30:00",
+        image: '/assets/images/events/pres.jpg',
+        scores: [4, 5]
     },
     {
         id: 5,
-        ensembleID: 3,
-        name: "President Concert",
+        name: "ABRSM Gr5. Violin Exam",
         description: "Event Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         type: "Performance",
         date: "2022-03-24",
         time: "15:45:00",
         location: { lat: 35.899526, lng: 14.518251 },
-        scores: [4, 5]
+        scores: [1, 2],
+        image: '/assets/images/events/abrsm.jpg'
     }
 ];
 
